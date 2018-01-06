e1924a822b6ab85ebac7fda41f68f40fc80ec1a0
Todd Gerdy <toadimous@gmail.com>
2018-01-05 22:59:39 -0500

    setting up the server file

---
+++ server.js
@@ -0,0 +1,12 @@
+`use strict`;
+
+const express = require('express');
+const app = express();
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
